1eb127cfe57307b0f021d57a5bf431b0b4957cf3
Marco Bertini <marco.bertini@unifi.it>
2024-05-14 19:28:15 +0200

    NEW: CLI tool to subsample large datasets to test the system

---
+++ random_subsample_dataset.py
@@ -0,0 +1,49 @@
+import os
+import shutil
+import random
+import argparse
+import glob
+
+# Define the arguments
+parser = argparse.ArgumentParser(description='Randomly subsample a dataset.')
+parser.add_argument('-i', '--source_directory', type=str, required=True, help='Source directory containing all the images')
+parser.add_argument('-o', '--destination_directory', type=str, required=True, help='Destination directory where the selected images will be copied')
+parser.add_argument('-n', '--num_images_to_copy', type=int, required=False, default=10000, help='Number of images to be copied')
+args = parser.parse_args()
+
+# Source directory containing all the images
+source_directory = args.source_directory
+
+# Destination directory where the selected images will be copied
+destination_directory = args.destination_directory
+
+# Number of images to be copied
+num_images_to_copy = args.num_images_to_copy
+
+print(f"Source directory: {source_directory}")
+print(f"Destination directory: {destination_directory}")
+print(f"Number of images to copy: {num_images_to_copy}")
+
+print("Reading image filenames...")
+# List all jpeg, jpg, and png files in the directory
+all_images = (glob.glob(os.path.join(source_directory, '*.[jJ][pP][gG]')) +
+              glob.glob(os.path.join(source_directory, '*.[jJ][pP][eE][gG]')) +
+              glob.glob(os.path.join(source_directory, '*.[pP][nN][gG]')))
+print(f"Number of images found: {len(all_images)}")
+
+print("Selecting images...")
+# Randomly select requested images
+selected_images = random.sample(all_images, num_images_to_copy)
+
+# Create the destination directory if it doesn't exist
+if not os.path.exists(destination_directory):
+    os.makedirs(destination_directory)
+
+print("Copying images...")
+# Copy selected images to the destination directory
+for image in selected_images:
+    source_path = os.path.join(source_directory, os.path.basename(image))
+    destination_path = os.path.join(destination_directory, os.path.basename(image))
+    shutil.copyfile(source_path, destination_path)
+
+print("Images copied successfully.")
